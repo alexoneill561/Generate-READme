cefd2456958e343bab816ce7609f096574c46eee
Alex O'Neill <alexoneill561@gmail.com>
2021-04-16 21:18:58 -0400

    finished read me

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# Generate-READme
-Craeting a quality readme that is easy to understand and will demonstrate and list how to use the readme and what is used in it.
+# generate readme
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  ## Description 
+  very descriptive readme generator
+
+  
+  View app [here]().
+
+
+  ## Table of Contents
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [License](#License)
+  * [Contributions](#Contributions)
+  * [Testing](#Testing)
+  * [Questions](#Questions)
+  
+
+  ## Installation
+  npm i
+
+
+  ## Usage
+  random
+
+
+  ## License
+  This project uses the MIT license.
+  
+  
+  ## Contributions
+  random
+
+
+  ## Testing
+  npm test
+
+
+  ## Questions
+  If you are confused or would like to ask me a question I am easily reached at alexoneill561@gmail.com . You can view all my projects at [undefined](https://github.com/alexoneill561/).
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,5 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+
 function renderLicenseBadge(license) {
   if (license === "MIT") {
     return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
@@ -10,25 +10,33 @@ function renderLicenseBadge(license) {
   } else if (license === "GPL 3.0") {
     return "[![License: GPL 3.0](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
   } else {
-    return " "
-}
+    return " "}
+  }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  // TODO: Create a function that returns the license link
+  
+  function renderLicenseLink(license) {
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+   }
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.projName}
+  // TODO: Create a function that returns the license section of README
+  
+  function renderLicenseSection(license) {
+
+   }
+
+  // TODO: Create a function to generate markdown for README
+  function generateMarkdown(data) {
+    return `# ${data.projName}
 
   ${renderLicenseBadge(data.license)}
   ## Description 
   ${data.description}
-  View the live application [here](${data.link}).
+
+  
+  View app [here](${data.link}).
+
+
   ## Table of Contents
   * [Installation](#Installation)
   * [Usage](#Usage)
@@ -36,23 +44,31 @@ function generateMarkdown(data) {
   * [Contributions](#Contributions)
   * [Testing](#Testing)
   * [Questions](#Questions)
-  ## Installation
-  ${data.install}
-  ## Usage
-  ${data.usage}
-  ## License
-  This project uses the ${data.license} license. 
-  ## Contributions
-  ${data.contribute}
-  ## Testing
-  ${data.tests}
-  ## Questions
-  If you are confused or would like to ask me a question I am easily reached at ${data.email} . You can view all my projects at [${data.username}](https://github.com/alexoneill561/).
-`;
-
   
 
-`;
+  ## Installation
+  ${data.install}
+
+
+  ## Usage
+  ${data.usage}
+
+
+  ## License
+  This project uses the ${data.license} license.
+  
+  
+  ## Contributions
+  ${data.contribute}
+
+
+  ## Testing
+  ${data.tests}
+
+
+  ## Questions
+  If you are confused or would like to ask me a question I am easily reached at ${data.email} . You can view all my projects at [${data.username}](https://github.com/alexoneill561/).
+  `;
 }
 
 module.exports = generateMarkdown;
